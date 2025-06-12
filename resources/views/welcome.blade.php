5ecd61c967ad11f962a6dfa1f303515da502e52f
CNTT7-phenikaa <23010362@st.phenikaa-uni.edu.vn>
2025-06-13 01:58:26 +0700

    chuyen huong ve trang chu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,18 @@
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
+                        @php
+                            $user = Auth::user();
+                            $redirectUrl = '#'; // fallback
+
+                            if ($user->role === 'admin') {
+                                $redirectUrl = route('admin.index');
+                            } elseif ($user->role === 'user') {
+                                $redirectUrl = route('user.index');
+                            }
+                        @endphp
                         <a
-                            href="{{ url('/dashboard') }}"
+                            href="{{ $redirectUrl }}"
                             class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
                         >
                             Dashboard
