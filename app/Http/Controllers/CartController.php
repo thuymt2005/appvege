016790efc564639e08ddd26eecb7442f19f9af0b
CNTT7-phenikaa <23010651@st.phenikaa-uni.edu.vn>
2025-06-16 14:00:32 +0700

    cartcontroller & orderscontroller

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        return view('user.cart');
+    }
+
+    // /**
+    //  * Add an item to the cart.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\RedirectResponse
+    //  */
+    // public function add(Request $request)
+    // {
+    //     // Logic to add item to cart
+    //     return redirect()->route('cart.index')->with('success', 'Item added to cart.');
+    // }
+
+    // /**
+    //  * Remove an item from the cart.
+    //  *
+    //  * @param  int  $itemId
+    //  * @return \Illuminate\Http\RedirectResponse
+    //  */
+    // public function remove($itemId)
+    // {
+    //     // Logic to remove item from cart
+    //     return redirect()->route('cart.index')->with('success', 'Item removed from cart.');
+    // }
+}
